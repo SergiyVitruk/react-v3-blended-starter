8ce4c7946af8fd126ed900f42b28eeba4da55ff0
SergiyVitruk <vitruk.sergiy@gmail.com>
2025-09-02 23:41:53 +0300

    ready task

---
+++ app/posts/[id]/PostDetails.client.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { useParams, useRouter } from 'next/navigation';
+import { useQuery } from '@tanstack/react-query';
+import css from './PostDetails.module.css';
+import { fetchPostById, fetchUserById } from '@/lib/api';
+import { useEffect, useState } from 'react';
+import { User } from '@/types/user';
+
+export default function PostDetailsClient() {
+  const { id } = useParams<{ id: string }>();
+
+  const [user, setUser] = useState<User | null>(null);
+
+  const router = useRouter();
+
+  const handleClickBack = () => {
+    router.back();
+  };
+
+  const { data } = useQuery({
+    queryKey: ['post', id],
+    queryFn: () => fetchPostById(id),
+    refetchOnMount: false,
+  });
+
+  useEffect(() => {
+    if (!data) return;
+    const fn = async () => {
+      const response = await fetchUserById(data.userId);
+      setUser(response);
+    };
+    fn();
+  }, [data]);
+
+  return (
+    <>
+      <div className={css.container}>
+        <div className={css.item}>
+          <button onClick={handleClickBack} className={css.backBtn}>
+            ← Back
+          </button>
+
+          {data && (
+            <div className={css.post}>
+              <div className={css.wrapper}>
+                <div className={css.header}>
+                  <h2>{data.title}</h2>
+                </div>
+
+                <p className={css.content}>{data.body}</p>
+              </div>
+              {user && <p className={css.user}>Author: {user.name}</p>}
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ app/posts/filter/[...slug]/page.tsx
@@ -0,0 +1,25 @@
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import { fetchPosts } from '@/lib/api';
+import PostsClient from './Posts.client';
+
+interface PostPageProps {
+  params: Promise<{ slug: string[] }>;
+}
+
+export default async function PostsPage({ params }: PostPageProps) {
+  const { slug } = await params;
+  const userId = slug[0];
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['posts', userId],
+    queryFn: () => fetchPosts({ searchText: '', page: 1, ...(userId !== 'All' && { userId }) }),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <PostsClient userId={userId} />
+    </HydrationBoundary>
+  );
+}
